1fdffcc298bfa10abf01bccc903d7316dec3a218
Adam Rivera <arivera@sovereignshare.com>
2015-09-02 21:04:42 -0500

    Lowered sync threshold to get data for smooth air.

--- Fly Smuthe/Fly Smuthe/TurbulenceStatisticModel.swift
+++ Fly Smuthe/Fly Smuthe/TurbulenceStatisticModel.swift
@@ -61,15 +61,15 @@ class TurbulenceStatisticModel {
             return true;
         }
         
-        if(XAccel != nil && turbulenceDataState.XAccel != nil && fabs(XAccel - turbulenceDataState.XAccel) > 0.1){
+        if(XAccel != nil && turbulenceDataState.XAccel != nil && fabs(XAccel - turbulenceDataState.XAccel) > 0.05){
             return true;
         }
         
-        if(YAccel != nil && turbulenceDataState.YAccel != nil && fabs(YAccel - turbulenceDataState.YAccel) > 0.1){
+        if(YAccel != nil && turbulenceDataState.YAccel != nil && fabs(YAccel - turbulenceDataState.YAccel) > 0.05){
             return true;
         }
         
-        if(ZAccel != nil && turbulenceDataState.ZAccel != nil && fabs(ZAccel - turbulenceDataState.ZAccel) > 0.1){
+        if(ZAccel != nil && turbulenceDataState.ZAccel != nil && fabs(ZAccel - turbulenceDataState.ZAccel) > 0.05){
             return true;
         }
         
